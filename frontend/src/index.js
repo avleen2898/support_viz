be16763e23badf903113dea2a43b9791f2fb73b9
Avleen Kaur <kaurx111@umn.edu>
2020-05-24 09:49:04 -0500

    Material UI setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <CssBaseline />
+      <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
