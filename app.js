e3cff64cba2d3e8012287c937888bc60dccf7edc
Suresh <suresh.siddharth@gmail.com>
2020-06-12 19:16:05 -0500

    Changes for Heroku

--- app.js
+++ app.js
@@ -54,4 +54,11 @@ app.use(function (err, req, res, next) {
     res.render('error');
 });
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    });
+}
+
 module.exports = app;
